b35b4738810bb12da2812a65f2a40458ab4105b8
Matheus Damásio <matheusgtdamasio@gmail.com>
2019-01-17 12:09:57 -0200

    Cliente, TipoCliente, Telefones, Telefones e Endereços

---
+++ src/main/java/com/matheusdamasio/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,13 @@
+package com.matheusdamasio.cursomc.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matheusdamasio.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
+
