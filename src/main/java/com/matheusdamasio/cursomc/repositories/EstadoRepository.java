116c63abbbbf41fa0ecdc64de7bf5e47b6c16d1d
Matheus Damásio <matheusgtdamasio@gmail.com>
2019-01-17 10:14:22 -0200

    Estado e Cidade

---
+++ src/main/java/com/matheusdamasio/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,12 @@
+package com.matheusdamasio.cursomc.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matheusdamasio.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
