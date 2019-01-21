dde6d8b9078d6d36f61be1f84ae4f4264bdaacaa
Matheus Damásio <matheusgtdamasio@gmail.com>
2019-01-21 11:38:17 -0200

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/matheusdamasio/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,12 @@
+package com.matheusdamasio.cursomc.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matheusdamasio.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
---
+++ src/main/java/com/matheusdamasio/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,12 @@
+package com.matheusdamasio.cursomc.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matheusdamasio.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
