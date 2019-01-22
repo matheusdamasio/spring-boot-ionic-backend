1986b565051dbf8635ad94a86d6666aff323a384
Matheus Damásio <matheusgtdamasio@gmail.com>
2019-01-22 10:50:16 -0200

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/matheusdamasio/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,12 @@
+package com.matheusdamasio.cursomc.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matheusdamasio.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
